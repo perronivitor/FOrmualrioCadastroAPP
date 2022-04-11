02ca88b203c7eaf91fd406447f4e644ed26b2f38
perronivitor <vitorope48@gmail.com>
2022-04-11 10:49:11 -0300

    Init project

---
+++ app/src/main/java/com/example/validacaoformulariosapp/ui/FormularioCadastroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.validacaoformulariosapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.validacaoformulariosapp.R
+
+class FormularioCadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_cadastro)
+    }
+}
