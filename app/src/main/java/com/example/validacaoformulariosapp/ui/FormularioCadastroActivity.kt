8d01dcaed2e0f6bd326d612684db61861884a520
perronivitor <vitorope48@gmail.com>
2022-04-11 11:40:16 -0300

    Criado layout formulario

--- app/src/main/java/com/example/validacaoformulariosapp/ui/FormularioCadastroActivity.kt
+++ app/src/main/java/com/example/validacaoformulariosapp/ui/FormularioCadastroActivity.kt
@@ -3,10 +3,12 @@ package com.example.validacaoformulariosapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.validacaoformulariosapp.R
+import com.google.android.material.textfield.TextInputLayout
 
 class FormularioCadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_cadastro)
+
     }
 }
